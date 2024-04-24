b70c3f4001c0c59c87de2ce169d3f3b29a21f200
Evan Anders <evanhanders@gmail.com>
2024-04-24 12:50:07 -0700

    puts back commented actstore tests

--- tests/unit/training/test_activations_store.py
+++ tests/unit/training/test_activations_store.py
@@ -179,136 +179,136 @@ def test_toy_activations_store__shapes_look_correct(
     assert buffer.device == store.device
 
 
-# # tests involving loading real models / real datasets are very slow
-# # so do lots of stuff in this one test to make each load of model / data count
-# def test_activations_store__shapes_look_correct_with_real_models_and_datasets(
-#     cfg: LanguageModelSAERunnerConfig, model: HookedTransformer
-# ):
-#     # --- first, test initialisation ---
+# tests involving loading real models / real datasets are very slow
+# so do lots of stuff in this one test to make each load of model / data count
+def test_activations_store__shapes_look_correct_with_real_models_and_datasets(
+    cfg: LanguageModelSAERunnerConfig, model: HookedTransformer
+):
+    # --- first, test initialisation ---
 
-#     store = LMActivationsStore.from_config(model, cfg)
+    store = LMActivationsStore.from_config(model, cfg)
 
-#     assert store.model == model
+    assert store.model == model
 
-#     assert isinstance(store.dataset, IterableDataset)
-#     assert isinstance(store.iterable_dataset, Iterable)
+    assert isinstance(store.dataset, IterableDataset)
+    assert isinstance(store.iterable_dataset, Iterable)
 
-#     # the rest is in the dataloader.
-#     expected_size = (
-#         cfg.store_batch_size * cfg.context_size * cfg.n_batches_in_buffer // 2
-#     )
-#     assert store.storage_buffer.shape == (expected_size, 1, cfg.d_in)
+    # the rest is in the dataloader.
+    expected_size = (
+        cfg.store_batch_size * cfg.context_size * cfg.n_batches_in_buffer // 2
+    )
+    assert store.storage_buffer.shape == (expected_size, 1, cfg.d_in)
 
-#     # --- Next, get batch tokens and assert they look correct ---
+    # --- Next, get batch tokens and assert they look correct ---
 
-#     batch = store.get_batch_tokens()
+    batch = store.get_batch_tokens()
 
-#     assert isinstance(batch, torch.Tensor)
-#     assert batch.shape == (
-#         store.store_batch_size,
-#         store.context_size,
-#     )
-#     assert batch.device == store.device
+    assert isinstance(batch, torch.Tensor)
+    assert batch.shape == (
+        store.store_batch_size,
+        store.context_size,
+    )
+    assert batch.device == store.device
 
-#     # --- Next, get activations and assert they look correct ---
+    # --- Next, get activations and assert they look correct ---
 
-#     activations = store.get_activations(batch)
+    activations = store.get_activations(batch)
 
-#     assert isinstance(activations, torch.Tensor)
-#     assert activations.shape == (
-#         store.store_batch_size,
-#         store.context_size,
-#         1,
-#         store.d_in,
-#     )
-#     assert activations.device == store.device
+    assert isinstance(activations, torch.Tensor)
+    assert activations.shape == (
+        store.store_batch_size,
+        store.context_size,
+        1,
+        store.d_in,
+    )
+    assert activations.device == store.device
 
-#     # --- Next, get buffer and assert it looks correct ---
+    # --- Next, get buffer and assert it looks correct ---
 
-#     n_batches_in_buffer = 3
-#     buffer = store.get_buffer(n_batches_in_buffer)
+    n_batches_in_buffer = 3
+    buffer = store.get_buffer(n_batches_in_buffer)
 
-#     assert isinstance(buffer, torch.Tensor)
-#     buffer_size_expected = (
-#         store.store_batch_size * store.context_size * n_batches_in_buffer
-#     )
+    assert isinstance(buffer, torch.Tensor)
+    buffer_size_expected = (
+        store.store_batch_size * store.context_size * n_batches_in_buffer
+    )
 
-#     assert buffer.shape == (buffer_size_expected, 1, store.d_in)
-#     assert buffer.device == store.device
-
-
-# def test_activations_store__get_activations_head_hook(ts_model: HookedTransformer):
-#     cfg = build_sae_cfg(
-#         hook_point="blocks.0.attn.hook_q",
-#         hook_point_head_index=2,
-#         hook_point_layer=1,
-#         d_in=4,
-#     )
-#     activation_store_head_hook = LMActivationsStore.from_config(ts_model, cfg)
-#     batch = activation_store_head_hook.get_batch_tokens()
-#     activations = activation_store_head_hook.get_activations(batch)
-
-#     assert isinstance(activations, torch.Tensor)
-#     assert activations.shape == (
-#         activation_store_head_hook.store_batch_size,
-#         activation_store_head_hook.context_size,
-#         1,
-#         activation_store_head_hook.d_in,
-#     )
-#     assert activations.device == activation_store_head_hook.device
+    assert buffer.shape == (buffer_size_expected, 1, store.d_in)
+    assert buffer.device == store.device
 
 
-# # 12 is divisible by the length of "hello world", 11 and 13 are not
-# @pytest.mark.parametrize("context_size", [11, 12, 13])
-# def test_activations_store__get_batch_tokens__fills_the_context_separated_by_bos(
-#     ts_model: HookedTransformer, context_size: int
-# ):
-#     assert ts_model.tokenizer is not None
-#     dataset = Dataset.from_list(
-#         [
-#             {"text": "hello world"},
-#         ]
-#         * 100
-#     )
-#     cfg = build_sae_cfg(
-#         store_batch_size=2,
-#         context_size=context_size,
-#     )
+def test_activations_store__get_activations_head_hook(ts_model: HookedTransformer):
+    cfg = build_sae_cfg(
+        hook_point="blocks.0.attn.hook_q",
+        hook_point_head_index=2,
+        hook_point_layer=1,
+        d_in=4,
+    )
+    activation_store_head_hook = LMActivationsStore.from_config(ts_model, cfg)
+    batch = activation_store_head_hook.get_batch_tokens()
+    activations = activation_store_head_hook.get_activations(batch)
 
-#     activation_store = LMActivationsStore.from_config(ts_model, cfg, dataset=dataset)
-#     encoded_text = tokenize_with_bos(ts_model, "hello world")
-#     tokens = activation_store.get_batch_tokens()
-#     assert tokens.shape == (2, context_size)  # batch_size x context_size
-#     all_expected_tokens = (encoded_text * ceil(2 * context_size / len(encoded_text)))[
-#         : 2 * context_size
-#     ]
-#     expected_tokens1 = all_expected_tokens[:context_size]
-#     expected_tokens2 = all_expected_tokens[context_size:]
-#     if expected_tokens2[0] != ts_model.tokenizer.bos_token_id:
-#         expected_tokens2 = [ts_model.tokenizer.bos_token_id] + expected_tokens2[:-1]
-#     assert tokens[0].tolist() == expected_tokens1
-#     assert tokens[1].tolist() == expected_tokens2
+    assert isinstance(activations, torch.Tensor)
+    assert activations.shape == (
+        activation_store_head_hook.store_batch_size,
+        activation_store_head_hook.context_size,
+        1,
+        activation_store_head_hook.d_in,
+    )
+    assert activations.device == activation_store_head_hook.device
 
 
-# def test_activations_store__get_next_dataset_tokens__tokenizes_each_example_in_order(
-#     ts_model: HookedTransformer,
-# ):
-#     cfg = build_sae_cfg()
-#     dataset = Dataset.from_list(
-#         [
-#             {"text": "hello world1"},
-#             {"text": "hello world2"},
-#             {"text": "hello world3"},
-#         ]
-#     )
-#     activation_store = LMActivationsStore.from_config(ts_model, cfg, dataset=dataset)
+# 12 is divisible by the length of "hello world", 11 and 13 are not
+@pytest.mark.parametrize("context_size", [11, 12, 13])
+def test_activations_store__get_batch_tokens__fills_the_context_separated_by_bos(
+    ts_model: HookedTransformer, context_size: int
+):
+    assert ts_model.tokenizer is not None
+    dataset = Dataset.from_list(
+        [
+            {"text": "hello world"},
+        ]
+        * 100
+    )
+    cfg = build_sae_cfg(
+        store_batch_size=2,
+        context_size=context_size,
+    )
 
-#     assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
-#         ts_model, "hello world1"
-#     )
-#     assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
-#         ts_model, "hello world2"
-#     )
-#     assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
-#         ts_model, "hello world3"
-#     )
+    activation_store = LMActivationsStore.from_config(ts_model, cfg, dataset=dataset)
+    encoded_text = tokenize_with_bos(ts_model, "hello world")
+    tokens = activation_store.get_batch_tokens()
+    assert tokens.shape == (2, context_size)  # batch_size x context_size
+    all_expected_tokens = (encoded_text * ceil(2 * context_size / len(encoded_text)))[
+        : 2 * context_size
+    ]
+    expected_tokens1 = all_expected_tokens[:context_size]
+    expected_tokens2 = all_expected_tokens[context_size:]
+    if expected_tokens2[0] != ts_model.tokenizer.bos_token_id:
+        expected_tokens2 = [ts_model.tokenizer.bos_token_id] + expected_tokens2[:-1]
+    assert tokens[0].tolist() == expected_tokens1
+    assert tokens[1].tolist() == expected_tokens2
+
+
+def test_activations_store__get_next_dataset_tokens__tokenizes_each_example_in_order(
+    ts_model: HookedTransformer,
+):
+    cfg = build_sae_cfg()
+    dataset = Dataset.from_list(
+        [
+            {"text": "hello world1"},
+            {"text": "hello world2"},
+            {"text": "hello world3"},
+        ]
+    )
+    activation_store = LMActivationsStore.from_config(ts_model, cfg, dataset=dataset)
+
+    assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
+        ts_model, "hello world1"
+    )
+    assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
+        ts_model, "hello world2"
+    )
+    assert activation_store._get_next_dataset_tokens().tolist() == tokenize_with_bos(
+        ts_model, "hello world3"
+    )
